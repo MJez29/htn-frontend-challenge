f5f3ca2e0834993bc560917ee29181a118207b83
Michal Jez <Michal.G.Jez@gmail.com>
2018-02-17 00:30:49 -0500

    Added times to schedule

--- src/components/schedule/schedule.jsx
+++ src/components/schedule/schedule.jsx
@@ -3,6 +3,10 @@ import Axios from "axios";
 
 import Event from "./event";
 
+import "./schedule.css";
+
+import Helper from "../../helper";
+
 class Schedule extends React.Component {
 
     static get HOUR_HEIGHT() {
@@ -17,7 +21,8 @@ class Schedule extends React.Component {
         // Gets the event data from the server
         Axios.get("https://hackthenorth.com/fe-schedule.json")
             .then((res) => {
-                //initSchedule(res.data);
+                this.initSchedule(res.data);
+                this.forceUpdate();
             })
             .catch((err) => {
                 console.log(err);
@@ -36,11 +41,61 @@ class Schedule extends React.Component {
             "created_at": "2018-09-15T04:00:59-04:00",
             "updated_at": "2018-09-15T04:00:59-04:00"
         }
+
+        this.initTimes = this.initTimes.bind(this);
+        this.initSchedule = this.initSchedule.bind(this);
+    }
+
+    /**
+     * Creates an array of React components that are aligned to display the times in the schedule timeline
+     */
+    initTimes() {
+        let scheduleTimes = [];
+
+        this.earliestTime.setMinutes(0, 0, 0);
+        this.latestTime = new Date(this.latestTime + 1000 * 60 * 60);
+        this.latestTime.setMinutes(0, 0, 0);
+
+        let n = Math.ceil((this.latestTime - this.earliestTime) / (1000 * 60 * 60));
+        console.log(this.earliestTime);
+        console.log(this.latestTime);
+        console.log(n);
+
+        for (let i = 0; i < n; i++) {
+            let t = new Date(this.earliestTime.getTime() + i * 60 * 60 * 1000);
+            
+            // If the start of a new day
+            if (t.getHours() == 0) {
+                scheduleTimes.push(
+                    <h1 className="schedule-day" style={ {
+                        top: `calc(${i * Schedule.HOUR_HEIGHT}px - 1.5em)`
+                    } }>
+                        { Helper.toWeekday(t.getDay()) }
+                    </h1>
+                )
+            } else {
+                scheduleTimes.push(
+                    <span className="schedule-time" style={ {
+                        top: `calc(${i * Schedule.HOUR_HEIGHT}px - 0.4em)`
+                    } }>
+                        { `${t.getHours()}:${ Helper.padTime(t.getMinutes())}` }
+                    </span>
+                );
+            }
+        }
+
+        this.timeColumn = (
+            <div className="schedule-times" style={ {
+                height: n * Schedule.HOUR_HEIGHT
+            } }>
+                { scheduleTimes }
+            </div>        
+        )
     }
 
     initSchedule(data) {
         this.earliestTime = new Date(8640000000000000);
-        let latestTime = new Date(-8640000000000000);
+        this.latestTime = new Date(-8640000000000000);
 
         this.scheduleData = [];
         for (let i = 0; i < data.length; ++i) {
@@ -64,20 +119,15 @@ class Schedule extends React.Component {
                 this.latestTime =this.scheduleData[i].endTime;
             }
         }
+
+        this.initTimes();
     }
 
     render() {
 
-
         return (
             <div className="schedule-container">
-                <table>
-                    <tr>
-                        <td className="schedule-times">
-                        
-                        </td>
-                    </tr>
-                </table>
+                { this.timeColumn }
             </div>
         );
     }
--- src/helper.js
+++ src/helper.js
@@ -5,6 +5,25 @@ let helper = {
         } else {
             return "" + n;
         }
+    },
+
+    toWeekday: (n) => {
+        switch (n) {
+            case 0:
+                return "Sun";
+            case 1: 
+                return "Mon";
+            case 2:
+                return "Tue";
+            case 3:
+                return "Wed";
+            case 4:
+                return "Thu";
+            case 5:
+                return "Fri";
+            case 6:
+                return "Sat";
+        }
     }
 }
 
