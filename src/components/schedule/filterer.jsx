c2c754c1223909e1195d3c1afe7c74a36cbc400f
Michal Jez <Michal.G.Jez@gmail.com>
2018-02-17 22:16:08 -0500

    Added colored tag filter checkboxes

--- src/components/schedule/filterer.jsx
+++ src/components/schedule/filterer.jsx
@@ -30,7 +30,63 @@ class Filterer extends React.Component {
                     <h1 className="add-filters-button" onClick={ this.toggleFilters }>
                         <i className="fa fa-minus"></i> Hide Filters
                     </h1>
-                    <div className="pure-g">
+                    <div className="pure-form pure-form-stacked">
+                        <fieldset>
+                            <div className="pure-control-group">
+                                <div className="pure-g">
+                                    <div className="pure-u-1-2 filter-label">
+                                        <label htmlFor="keyword">Filter by Keyword</label>
+                                    </div>
+                                    <div className="pure-u-1-2">
+                                        <input type="text" id="keyword" name="keyword" />
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="pure-control-group">
+                                <div className="pure-g">
+                                    <div className="pure-u-1-2 filter-label">
+                                        <label htmlFor="tags">Filter by Tags</label>
+                                    </div>
+                                    <div className="pure-u-1-2">
+                                    <label className="checkbox-container checkbox-lightning_challenge">Lightning Challenge
+                                        <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="lightning_challenge" id=""/> <br/>
+                                        <span class="checkbox"></span>
+                                    </label>
+                                    <label className="checkbox-container checkbox-talk">Talk
+                                        <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="talk" id=""/> <br/>
+                                        <span class="checkbox"></span>
+                                    </label>
+                                    <label className="checkbox-container checkbox-logistics">Logistics
+                                        <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="logistics" id=""/> <br/>
+                                        <span class="checkbox"></span>
+                                    </label>
+                                    <label className="checkbox-container checkbox-workshop">Workshop
+                                        <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="workshop" id=""/> <br/>
+                                        <span class="checkbox"></span>
+                                    </label>
+                                    <label className="checkbox-container checkbox-judging">Judging
+                                        <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="judging" id=""/> <br/>
+                                        <span class="checkbox"></span>
+                                    </label>
+                                    <label className="checkbox-container checkbox-food">Food
+                                        <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="food" id=""/> <br/>
+                                        <span class="checkbox"></span>
+                                    </label>
+                                    <label className="checkbox-container checkbox-meetup">Meetup
+                                        <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="meetup" id=""/>
+                                        <span class="checkbox"></span>
+                                    </label>
+                                    
+                                    
+                            
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </fieldset>
+                    </div>
+                    {/* <div className="pure-g">
                         <div className="pure-u-2-5">
                             <span>Filter by Keyword</span>
                         </div>
@@ -51,7 +107,7 @@ class Filterer extends React.Component {
                             <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="food" id=""/> Food <br/>
                             <input type="checkbox" defaultChecked onChange={ this.props.onTagChange } name="meetup" id=""/> Meetup
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             );
         } else {
